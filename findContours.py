9db7d53ddfe05834b486f800db630c6faea016cd
UNNC_Corey.H <corey.huang0103@gmail.com>
2024-01-11 19:40:02 +0800

    Add comment

--- findContours.py
+++ findContours.py
@@ -5,16 +5,16 @@ import numpy as np
 def findContours(imgTresh: np.ndarray, original_img: np.ndarray) -> np.ndarray:
     contours, hierarchy = cv2.findContours(
         imgTresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
-    )
-    imgContours = original_img.copy()
+    ) # Find the contours
+    imgContours = original_img.copy() # Copy the original image
     for cnt in contours:
-        area = cv2.contourArea(cnt)
+        area = cv2.contourArea(cnt) # Calculate the area of the contour
         if area > 1200:
-            # cv2.drawContours(imgContours, cnt, -1, (255, 0, 0), 3)
-            peri = cv2.arcLength(cnt, True)
-            approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
-            x, y, w, h = cv2.boundingRect(approx)
-            cv2.rectangle(imgContours, (x, y - 20), (x + w, y + h + 10), (0, 255, 0), 2)
+            # cv2.drawContours(imgContours, cnt, -1, (255, 0, 0), 3) # Draw the contours
+            peri = cv2.arcLength(cnt, True) # Calculate the perimeter of the contour
+            approx = cv2.approxPolyDP(cnt, 0.02 * peri, True) # Approximate the shape of the contour
+            x, y, w, h = cv2.boundingRect(approx) # Get the bounding rectangle of the contour
+            cv2.rectangle(imgContours, (x, y - 20), (x + w, y + h + 10), (0, 255, 0), 2) # Draw the bounding rectangle
             cv2.putText(
                 imgContours,
                 "Apple",
@@ -23,5 +23,5 @@ def findContours(imgTresh: np.ndarray, original_img: np.ndarray) -> np.ndarray:
                 1.5,
                 (0, 255, 0),
                 1,
-            )
+            ) # Put the text "Apple" on the image
     return imgContours
--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from findContours import findContours
 def main():
     img = cv2.imread("apple.png")
     # cv2.imshow("Original", img)
-    imgTresh = preProcessing(img)
+    imgTresh = preProcessing(img) # Preprocess the image
     # cv2.imshow("Preprocessed", imgTresh)
-    imgContours = findContours(imgTresh, img)
+    imgContours = findContours(imgTresh, img) # Find the contours
     cv2.imshow("Contours", imgContours)
     k = cv2.waitKey(0)
     if k == 27:
